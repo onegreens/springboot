36ff0d772b24a8503b633702db6fb976f2b23d3d
895645094@qq.com <ll12345678>
2018-08-31 18:12:16 +0800

    add war

--- learn_springboot_all/learn_springboot_1/src/main/java/com/example/learn_springboot_more/LearnSpringbootMoreApplication.java
+++ learn_springboot_all/learn_springboot_1/src/main/java/com/example/learn_springboot_more/LearnSpringbootMoreApplication.java
@@ -3,11 +3,19 @@ package com.example.learn_springboot_more;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-public class LearnSpringbootMoreApplication {
+public class LearnSpringbootMoreApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(LearnSpringbootMoreApplication.class);
+    }
+
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(LearnSpringbootMoreApplication.class, args);
     }
+
 }
